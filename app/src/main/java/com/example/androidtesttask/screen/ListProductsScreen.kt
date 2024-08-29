5d266287e9e04a063721a26168c0edf8f9bb422d
Konstantin <konstload@gmail.com>
2024-08-30 00:10:42 +0300

    Поле поиска

--- app/src/main/java/com/example/androidtesttask/screen/ListProductsScreen.kt
+++ app/src/main/java/com/example/androidtesttask/screen/ListProductsScreen.kt
@@ -18,6 +18,8 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilterChip
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
