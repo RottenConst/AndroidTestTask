975033bfa432b46c973467bc375b9c6e499cd7cb
Konstantin <konstload@gmail.com>
2024-08-30 00:17:39 +0300

    Цвета

--- app/src/main/java/com/example/androidtesttask/screen/ListProductsScreen.kt
+++ app/src/main/java/com/example/androidtesttask/screen/ListProductsScreen.kt
@@ -1,5 +1,6 @@
 package com.example.androidtesttask.screen
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
@@ -8,6 +9,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Create
@@ -32,10 +34,12 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.androidtesttask.R
 import com.example.androidtesttask.data.Item
@@ -78,8 +82,7 @@ fun TopBar(title: String) {
             )
         },
         colors = TopAppBarDefaults.topAppBarColors(
-            containerColor = Color(0xFFB1DCFC),
-            titleContentColor = Color.Black
+            containerColor = Blue,
         )
     )
 }
