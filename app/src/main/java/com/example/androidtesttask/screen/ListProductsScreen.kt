218ee5cd2f305a8c06f8e94fa0e8cdc48b5a25be
Konstantin <konstload@gmail.com>
2024-08-30 00:30:15 +0300

    Финальная коректировка и дополнение

--- app/src/main/java/com/example/androidtesttask/screen/ListProductsScreen.kt
+++ app/src/main/java/com/example/androidtesttask/screen/ListProductsScreen.kt
@@ -31,6 +31,10 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
